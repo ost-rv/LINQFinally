7b774643157c2c5207e383361f81f1a71f928ad7
ost-rv <ost-rv@mail.ru>
2021-12-25 20:16:02 +0300

    Add project files.

---
+++ LINQFinally/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+{
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+            Console.ReadKey();
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var result = classes.SelectMany(c => c.Students).ToArray();
+
+            return result; 
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
